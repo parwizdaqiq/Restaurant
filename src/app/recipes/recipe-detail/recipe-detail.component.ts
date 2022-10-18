908a5aebdf9673fc0239c396331f8f431c9a7ab0
parwizdaqiq <daqiqparwiz@gmail.com>
2022-10-19 01:30:20 +0500

    working on shopping list components

---
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-recipe-detail',
+  templateUrl: './recipe-detail.component.html',
+  styleUrls: ['./recipe-detail.component.css']
+})
+export class RecipeDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
